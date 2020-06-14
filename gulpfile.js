b1986849c52c68e55e8a5b844b30ae5d387f969a
Lyubov <spiridonova73l@gmail.com>
2020-06-14 21:12:04 +0400

    fixed html, styles and added form script

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const browsersync = require('browser-sync');
 
-const dist = './docs/';
+// const dist = './docs/';
+const dist = '/Applications/MAMP/htdocs/picture';
 
 gulp.task('copy-html', () => {
   return gulp
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,18 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   modals();
-  sliders('.js-feedback-slider-item', '', '.js-main-prev', '.js-main-next');
+  sliders(
+    '.js-feedback-slider-item',
+    '',
+    '.js-main-prev',
+    '.js-main-next',
+    30000,
+  );
   sliders('.js-main-slider-item', 'vertical');
+  forms();
 });
